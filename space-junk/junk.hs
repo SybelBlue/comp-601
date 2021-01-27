d22233cd2e2fc0850249365ec0b90c36c3e53346
L Caraco <lbc.dna@gmail.com>
2021-01-26 18:44:02 -0600

    cleaned junk

--- space-junk/junk.hs
+++ space-junk/junk.hs
@@ -1,20 +1,10 @@
 import Data.Tuple (fst, snd)
-import Data.List (elemIndex)
 
-readIntLine :: IO Int
-readIntLine = read <$> getLine
-
-readInts :: IO [Int]
-readInts = map read <$> words <$> getLine
-
-minIndex :: [Int] -> Int
-minIndex [] = -1
-minIndex (x:xs) = fst $ foldr folder (0, x) $ zip [1..] xs
-    where folder a b = if snd b < snd a then b else a
+minDay :: [Int] -> Int
+minDay [] = 0
+minDay (x:xs) = fst . foldr least (1, x) $ zip [2..] xs
+    where least a b = if snd b < snd a then b else a
 
 main :: IO ()
-main = do
-    days <- readIntLine
-    junkLevels <- readInts
-    let goodDay = 1 + minIndex (take days junkLevels)
-    print goodDay
+main = print =<< minDay . map read . words <$> (getLine *> getLine)
+-- the first input is the number of numbers in the next line, uneccessary
